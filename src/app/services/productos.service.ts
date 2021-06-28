0d8f4fe43609088e9ad194379a5a6e44a5a1b3ed
AxelFunes <chakyfunes@gmail.com>
2021-06-28 01:10:34 -0300

    1 file modified

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,7 +8,6 @@ import {
 import { of } from 'rxjs';
 import { Producto } from '../models/producto';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -16,16 +15,13 @@ export class ProductosService {
   resourceUrl: string;
   constructor(private httpClient: HttpClient) {
     this.resourceUrl = 'https://pymesbackend.azurewebsites.net/api/productos/';
-    //this.resourceUrl = 'https://localhost:44349/api/articulos/';
   }
 
   get() {
-    let params = new HttpParams();
-    return this.httpClient.get(this.resourceUrl, { params: params });
+    return this.httpClient.get(this.resourceUrl);
   }
 
   post(obj: Producto) {
     return this.httpClient.post(this.resourceUrl, obj);
   }
-
 }
